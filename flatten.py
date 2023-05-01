3c51cd50cdaa0421df1a571dbc5396ac1c11729b
jungheeeun <jungheeeun272@gmail.com>
2023-05-01 22:33:50 +0900

    재귀적으로 호출되는 flatten 함수가 입력으로다차원 리스트를 일차원 리스트로 변환

---
+++ flatten.py
@@ -0,0 +1,17 @@
+#재귀적으로 호출되는 flatten 함수가 입력으로 주어진 다차원 리스트를 일차원 리스트로 변환
+example = [[1,2,3 ] , [4,[5,6]],7, [8,9]]
+
+def flatten(data):
+    output = []
+    for item in data:
+        if type(item) == list:
+            # 재귀적으로 처리 
+            output += flatten(item)
+        else:
+            output.append(item)
+    return output
+
+print(flatten(example))
+# python3.10 flatten.py 
+# 파이썬 !!! 
+
